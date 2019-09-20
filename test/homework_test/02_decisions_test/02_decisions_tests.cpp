ebad559aca2e4066c27d08921742cf7b635ebf02
Art Gonzalez <arturo.gonzalez@austincc.edu>
2019-09-19 22:02:30 -0500

    Minor correction in switch and test case.

--- test/homework_test/02_decisions_test/02_decisions_tests.cpp
+++ test/homework_test/02_decisions_test/02_decisions_tests.cpp
@@ -32,10 +32,10 @@ TEST_CASE("Test get letter grade with if")
 }
 TEST_CASE("Test get letter grade with switch")
 {
-	REQUIRE(get_letter_grade_using_if(95) == "A");
-	REQUIRE(get_letter_grade_using_if(85) == "B");
-	REQUIRE(get_letter_grade_using_if(75) == "C");
-	REQUIRE(get_letter_grade_using_if(65) == "D");
-	REQUIRE(get_letter_grade_using_if(50) == "F");
+	REQUIRE(get_letter_grade_using_switch(95) == "A");
+	REQUIRE(get_letter_grade_using_switch(85) == "B");
+	REQUIRE(get_letter_grade_using_switch(75) == "C");
+	REQUIRE(get_letter_grade_using_switch(65) == "D");
+	REQUIRE(get_letter_grade_using_switch(50) == "F");
 
 }

6819a34a87647310ccc224f688cd5bb111eb8fe8
Dakari Antoine <dakari.antoine@g.austincc.edu>
2019-10-14 19:00:19 -0500

    Checking Account example

--- src/examples/04_module/01_bank/bank_account.h
+++ src/examples/04_module/01_bank/bank_account.h
@@ -1,20 +1,24 @@
 //bank_account.h
 #include <iostream>
-#ifndef BANK_ACCOUNT_H
+#ifndef BANK_ACCOUNT_H //header guards to prevent class redefinition error
 #define BANK_ACCOUNT_H
 class BankAccount
 {
 public:
 	BankAccount();
 	BankAccount(int b) : balance(b) {};//inline constructor
+	int get_balance() const; //const makes class members read only
 	void deposit(int amount);
 	void withdraw(int amount);
-	int get_balance() const; //const makes class members read only
+
 	friend void display(const BankAccount& account);
 	friend std::ostream & operator << (std::ostream & out,
 		const BankAccount & b);
 	friend std::istream & operator >> (std::istream & in, BankAccount & b);
-private: 
+protected:
+	
+	
+
 	int balance;
 };
 
--- src/examples/04_module/01_bank/main.cpp
+++ src/examples/04_module/01_bank/main.cpp
@@ -3,16 +3,13 @@
 #include "atm.h"
 #include "bank_account.h"
 #include "customer.h"
+#include "checking_account.h"
 using std::cout; using std::vector; using std::cin;
 
 int main()
 {
-	Customer customer;
-	customer.add_account(BankAccount(500));
-	customer.add_account(BankAccount(1500));
-	customer.add_account(BankAccount(5000));
-
-	cout << customer;
+	CheckingAccount a(1500);
+	cout << a.get_balance();
 
 	return 0;
 }

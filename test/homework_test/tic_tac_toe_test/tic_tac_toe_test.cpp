d48403281675b9288eac8a6e5a05ac2dbc79a47a
Art Gonzalez <arturo.gonzalez@austincc.edu>
2019-10-20 21:18:23 -0500

    HW 7 grading

--- test/homework_test/tic_tac_toe_test/tic_tac_toe_test.cpp
+++ test/homework_test/tic_tac_toe_test/tic_tac_toe_test.cpp
@@ -163,14 +163,23 @@ TEST_CASE("Test for no winner")
 {
 	TicTacToe board;
 	board.start_game("X");
+	REQUIRE(board.game_over() == false);
 	board.mark_board(1);//X         
+	REQUIRE(board.game_over() == false);
 	board.mark_board(2);//O          
+	REQUIRE(board.game_over() == false);
 	board.mark_board(3);//X          
+	REQUIRE(board.game_over() == false);
 	board.mark_board(5);//O          
+	REQUIRE(board.game_over() == false);
 	board.mark_board(4);//X 
+	REQUIRE(board.game_over() == false);
 	board.mark_board(7);//O    
+	REQUIRE(board.game_over() == false);
 	board.mark_board(9);//X 
+	REQUIRE(board.game_over() == false);
 	board.mark_board(6);//O    
+	REQUIRE(board.game_over() == false);
 	board.mark_board(8);//X 
 
 

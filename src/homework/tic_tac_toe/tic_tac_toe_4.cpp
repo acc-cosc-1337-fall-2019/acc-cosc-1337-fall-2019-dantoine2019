4ec70de1e1381e265959e54ee6424347da6b9949
dantoine2019 <dakari.antoine@g.austincc.edu>
2019-10-31 16:55:30 -0500

    Implemented code TicTacToe4

--- src/homework/tic_tac_toe/tic_tac_toe_4.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe_4.cpp
@@ -10,7 +10,10 @@ Win by column if and return true if (each column index)
 else
 false
 */
-
+bool TicTacToe4::check_column_win()
+{
+	return false;
+}
 
 
 
@@ -22,7 +25,10 @@ Win by row if
 8, 9, 10, 11 are equal 
 12,13,14, 15 are equal
 */
-
+bool TicTacToe4::check_row_win()
+{
+	return false;
+}
 
 
 /*
@@ -34,3 +40,7 @@ Win diagonally
 12,13,14, 15
 
 */
+bool TicTacToe4::check_diagonal_win()
+{
+	return false;
+}

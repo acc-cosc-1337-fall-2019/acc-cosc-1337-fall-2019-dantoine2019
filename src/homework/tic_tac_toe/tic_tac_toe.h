b326f61f35740f390347e8f239d579abed077fca
Dakari Antoine <dakari.antoine@g.austincc.edu>
2019-11-06 18:01:04 -0600

    Homework 10 solution ptr

--- src/homework/tic_tac_toe/tic_tac_toe.h
+++ src/homework/tic_tac_toe/tic_tac_toe.h
@@ -1,10 +1,11 @@
 //h
+#ifndef TIC_TAC_TOE_H
+#define TIC_TAC_TOE_H
 #include <vector>
 #include <string>
 #include <iostream>
 
-#ifndef TIC_TAC_TOE_H
-#define TIC_TAC_TOE_H
+
 
 using std::string; using std::vector; using std::ostream;
 
@@ -24,9 +25,9 @@ public:
 	int board_size;
 protected:
 	vector<string> pegs;
-	virtual bool check_column_win();
-	virtual bool check_row_win();
-	virtual bool check_diagonal_win();
+	virtual bool check_column_win()= 0;
+	virtual bool check_row_win()= 0;
+	virtual bool check_diagonal_win()= 0;
 
 private:
 	void set_next_player();
